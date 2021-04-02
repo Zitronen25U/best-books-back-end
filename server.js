ca3b572cc92d718f180b9f90ce0147ceb166ffb0
daneng1 <engeldb@gmail.com>
2021-04-01 18:23:20 -0700

    added book changes to backend

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 require('dotenv').config();
 
 const express = require('express');
-
+const Books = require('./Models/books');
 const cors = require('cors');
 
 const app = express();
@@ -13,8 +13,6 @@ app.use(cors());
 const PORT = process.env.PORT;
 
 const mongoose = require('mongoose');
-
-const User = require('./modules/User')
 
 
 mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true });
@@ -27,11 +25,10 @@ db.once('open', function () {
 
 app.use(express.json());
 
-const dan = new User({
-  email: 'engeldb@gmail.com',
-  books: [{name: '1984', description: 'Such a good read', status: 'my fav books'}, {name: 'Let my People Surf', description: 'Surfing is good for you', status: 'my fav books'}, {name: 'Grapes of Wrath', description: 'Grapes taste good', status: 'my fav books'}]
-});
-
+// const dan = new User({
+//   email: 'engeldb@gmail.com',
+//   books: [{name: '1984', description: 'Such a good read', status: 'my fav books'}, {name: 'Let my People Surf', description: 'Surfing is good for you', status: 'my fav books'}, {name: 'Grapes of Wrath', description: 'Grapes taste good', status: 'my fav books'}]
+// });
 
 app.get('/entries', (rec, res) => {
   dan.save();
@@ -39,63 +36,14 @@ app.get('/entries', (rec, res) => {
   res.send('entries');
 });
 
+app.get('/books', Books.getUser);
+app.post('/books', Books.addABook);
+app.delete('/books/:index', Books.deleteABook);
+app.put('/books/:index', Books.updateABook);
 
-app.post('/books', addABook);
-app.delete('/books/:index', deleteABook);
-
-function addABook(request, response){
-  console.log('inside of addABook', request.body);
-  const name = request.body.name;
-  const book = { name: request.body.bookName }
-
-  User.findOne({ email: name }, (err, entry) => {
-    if(err) return console.error(err);
-    if(!entry){
-      return console.error('user not found');
-    }
-    entry.books.push(book);
-    entry.save();
-    response.status(200).send(entry.books);
-  })
-}
-
-
-const brian = new User({
-  email: 'bethelemons@gmail.com',
-  books: [{name: 'The Hobbit', description: 'Bilbao Baggins discovers the truth', status: 'my fav books'}, {name: 'The Witcher', description: 'A Monster Hunter', status: 'my fav books'}, {name: 'The Martian', description: 'Surviving on Mars', status: 'my fav books'}]
-});
-
-
-app.get('/books', getUser);
-
-async function getUser(request, response) {
-  const name = request.query.user_name;
-  // console.log(request.query);
-  await User.find({ email: name }, function (err, items) {
-    if (err) return console.error(err);
-    console.log(items);
-    response.status(200).send(items[items.length -1].books);
-  })
-}
-
-function deleteABook(request, response) {
-  const index = request.params.index;
-  const userName = request.query.name;
-  // console.log("we are in deleteABook", index, userName);
-  
-  User.findOne({ email: userName }, (err, entry) => {
-    if (err) return err;
-    // console.log("this is our entry", entry);
-    const newBookArray = entry.books.filter((book, i) => {
-      // console.log(entry.books[i]);
-      return i !== index;
-    });
-    console.log("this is our newBookArray", newBookArray);
-    entry.books = newBookArray;
-    entry.save();
-    response.status(200).send('success!')
-  })
-}
-
+// const brian = new User({
+//   email: 'bethelemons@gmail.com',
+//   books: [{name: 'The Hobbit', description: 'Bilbao Baggins discovers the truth', status: 'my fav books'}, {name: 'The Witcher', description: 'A Monster Hunter', status: 'my fav books'}, {name: 'The Martian', description: 'Surviving on Mars', status: 'my fav books'}]
+// });
 
 app.listen(PORT, () => console.log(`server is up on ${PORT}`));
